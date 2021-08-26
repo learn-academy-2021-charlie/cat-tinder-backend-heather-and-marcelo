f3126dc523c914b0f15b31e51b581ad5c551d05a
celoazevedo <meazevedo86@gmail.com>
2021-08-26 11:16:45 -0700

    create test for index method, create index method. test pass

--- spec/requests/turtles_spec.rb
+++ spec/requests/turtles_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "Turtles", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of turtles" do
+      Turtle.create name: 'Another name', age: 5, enjoys: 'Walks'
+
+      # Make a request
+      get '/turtles'
+
+      turtle = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(turtle.length).to eq 1
+    end
   end
 end
