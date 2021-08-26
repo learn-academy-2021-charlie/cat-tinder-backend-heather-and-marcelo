fa41d9a75884cfe59264e6654be9dc32b11823d8
hroulston <h_roulston@live.com>
2021-08-26 15:36:00 -0700

    add appropriate request spec that looks for 422 error if validations are not met. add appropriate request validations to ensure API is sending useful information to front end developoer.

--- spec/requests/turtles_spec.rb
+++ spec/requests/turtles_spec.rb
@@ -23,14 +23,14 @@ RSpec.describe "Turtles", type: :request do
           enjoys: "Pizza"
         }
       }
-    
+
     post '/turtles', params: turtle_params
     new_turtle = Turtle.first
     expect(response).to have_http_status(200)
     expect(new_turtle.name).to eq "Michelangelo"
     expect(new_turtle.age).to eq 35
     expect(new_turtle.enjoys).to eq "Pizza"
-    
+
     end
   end
 
@@ -43,9 +43,9 @@ RSpec.describe "Turtles", type: :request do
           enjoys: "Pizza"
         }
       }
-    
+
     post '/turtles', params: turtle_params
-    
+
     updated_turtle_params = {
       turtle: {
         name: "Leonardo",
@@ -65,7 +65,7 @@ RSpec.describe "Turtles", type: :request do
     expect(updated_turtle.name).to eq "Leonardo"
     expect(updated_turtle.age).to eq 15
     expect(updated_turtle.enjoys).to eq "Justice"
-    
+
     end
   end
 
@@ -78,9 +78,9 @@ RSpec.describe "Turtles", type: :request do
           enjoys: "Pizza"
         }
       }
-    
+
     post '/turtles', params: turtle_params
-    
+
     turtle = Turtle.first
     delete "/turtles/#{turtle.id}"
 
@@ -88,6 +88,40 @@ RSpec.describe "Turtles", type: :request do
     turtles = Turtle.all
     expect(turtles).to be_empty
     end
+  end
+
+  describe 'cannot create a turtle with out valid attributes' do
+    it 'cannot create a turtle with out a name' do
+      turtle_params = {
+        turtle:  {
+          age: 35,
+          enjoys: "Pizza"
+        }
+      }
+      post '/turtles', params: turtle_params
+      turtle = JSON.parse(response.body)
+
+      expect(response).to have_http_status(422)
+      expect(turtle['name']).to include "can't be blank"
+
+    end
+
+    it 'cannot create a turtle with out an age' do
+      turtle_params = {
+        turtle:  {
+          name: "Michelangelo",
+          enjoys: "Pizza"
+        }
+      }
+      post '/turtles', params: turtle_params
+      turtle = JSON.parse(response.body)
+
+      expect(response).to have_http_status(422)
+      expect(turtle['age']).to include "can't be blank"
+
+    end
+
+
   end
 
 end
